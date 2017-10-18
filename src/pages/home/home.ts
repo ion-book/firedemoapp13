d2bb65f7ac0ff25b0f8aba61d3b25ec31b65bc42
Carlos Rojas <ing.carlosandresrojas@gmail.com>
2017-10-18 17:10:50 -0500

    Finalizado

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { FirebaseAnalytics } from '@ionic-native/firebase-analytics';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, private firebaseAnalytics: FirebaseAnalytics) {
+    
   }
 
+  ionViewDidLoad(){
+    console.log("IonViewDidLoad");
+    this.firebaseAnalytics.logEvent('page_view', {page: "home"})
+    .then((res: any) => console.log("Respuesta: " + res))
+    .catch((error: any) => console.error("Error: " + error));
+  }
 }
